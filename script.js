e1bb3a7f44538986d60c76e935f003e2d99882c1
guilhermecampos-dev <guilherme.campos.dev@gmail.com>
2024-03-25 22:39:12 -0300

    add a value rules

--- script.js
+++ script.js
@@ -2,8 +2,12 @@ function generatorNumber() {
 
     const min = Math.ceil(document.querySelector(".input-min").value)
     const max = Math.floor(document.querySelector(".input-max").value)
+ 
+    if(min >= max){
+        alert("Valor minimo tem quer ser menor que o máximo")
+    }else{
 
     let result = Math.floor(Math.random() * (max - min + 1)) + min;
     alert(result)
-
+    }
 }
